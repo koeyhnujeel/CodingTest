68583be2eaaad9973dd484b0d8e39ecf1c06d3cc
zunza <fksfks015@naver.com>
2023-10-02 01:25:10 +0900

    중복문자제거

---
+++ src/String/중복문자제거.java
@@ -0,0 +1,20 @@
+package String;
+
+import java.util.Scanner;
+
+public class 중복문자제거 {
+	static String solution(String str) {
+		String result = "";
+		for (int i = 0; i < str.length(); i++) {
+			if(i == str.indexOf(str.charAt(i))) result += str.charAt(i);
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+
+		System.out.println(solution(str));
+	}
+}
